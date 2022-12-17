df17de9f60fce2e3408bbe059702e5eab12a88e1
Simon <simon@farmleap.com>
2022-12-17 10:08:25 +0100

    initial commit

---
+++ src/main/java/com/mspr/arosaje/ARosaJeApplication.java
@@ -0,0 +1,13 @@
+package com.mspr.arosaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARosaJeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ARosaJeApplication.class, args);
+	}
+
+}
