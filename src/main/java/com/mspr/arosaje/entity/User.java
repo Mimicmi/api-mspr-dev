6c31985263c06d62d612922dd5ac0f17e752d66c
Simon <simon@farmleap.com>
2023-01-07 13:28:39 +0100

    feature/added user table

---
+++ src/main/java/com/mspr/arosaje/entity/User.java
@@ -0,0 +1,62 @@
+package com.mspr.arosaje.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {   
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_generator")
+    private int id;
+    private String pseudo;
+    private String password;
+    private String email;
+
+
+    public User() {
+    }
+
+    public User(int id, String pseudo, String password, String email) {
+        this.id = id;
+        this.pseudo = pseudo;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return this.pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
---
+++ src/main/java/com/mspr/arosaje/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.mspr.arosaje.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mspr.arosaje.entity.User;
+
+public class UserRepository extends JpaRepository<User, Integer>{
+    
+}
