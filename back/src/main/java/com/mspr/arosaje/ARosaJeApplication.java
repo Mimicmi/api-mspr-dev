51b144baa9588475c79fbb6b421d0a9df827372b
Simon <simon@farmleap.com>
2023-02-04 09:26:52 +0100

    WIP auth

--- back/src/main/java/com/mspr/arosaje/ARosaJeApplication.java
+++ back/src/main/java/com/mspr/arosaje/ARosaJeApplication.java
@@ -2,9 +2,9 @@ package com.mspr.arosaje;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+// import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class ARosaJeApplication {
 
 	public static void main(String[] args) {
