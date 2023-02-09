6a995650bd789c4ff50c61c9f5d3020656145461
Simon <simon@farmleap.com>
2023-02-09 10:11:50 +0100

    quick add cross origin

--- back/src/main/java/com/mspr/arosaje/controller/UserController.java
+++ back/src/main/java/com/mspr/arosaje/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,6 +50,7 @@ public class UserController {
         return new ResponseEntity<>(userData.get(), HttpStatus.OK);
     }
 
+    @CrossOrigin
     @PostMapping("users")
     public ResponseEntity<HttpStatus> createUser(@RequestBody User user) {
         String encodedPassword = passwordEncoder.encode(user.getPassword());
