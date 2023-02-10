4dab44a01ceb4d67895130676e8d0342e9f2ba9a
Simon <simon@farmleap.com>
2023-02-10 17:33:07 +0100

    fix/ turn off allowCredentials: true

--- back/src/main/java/com/mspr/arosaje/security/SecurityConfig.java
+++ back/src/main/java/com/mspr/arosaje/security/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+                //.allowCredentials(true);
     }
 
     @Bean
