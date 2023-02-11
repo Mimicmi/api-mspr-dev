32d9f4c04c9bf25e08e12c30c92526e078593f28
Simon <simon@farmleap.com>
2023-02-11 12:25:54 +0100

    feature/ajout de la recherche de plantes par rapport au userId

--- back/src/main/java/com/mspr/arosaje/repository/PlantRepository.java
+++ back/src/main/java/com/mspr/arosaje/repository/PlantRepository.java
@@ -1,9 +1,11 @@
 package com.mspr.arosaje.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mspr.arosaje.entity.Plant;
 
 public interface PlantRepository extends JpaRepository<Plant, Integer>{
-    
+    List<Plant> findByClientId(Integer client);
 }
