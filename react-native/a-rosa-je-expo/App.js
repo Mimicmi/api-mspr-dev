693d3e934d24421fbe90f934d14f0302cdfa3c3b
oussevo <m.bouazizi@epsi.fr>
2023-03-01 23:58:26 +0100

    its alive

--- react-native/a-rosa-je-expo/App.js
+++ react-native/a-rosa-je-expo/App.js
@@ -1,9 +1,8 @@
 // import "react-native-gesture-handler";
-import PlantesNavigator from "./routes/myPlantsStack";
 import RoutesNavigator from './routes/RoutesStack';
 
 export default function App() {
-  return 
-  ( <> <RoutesNavigator />
-      <PlantesNavigator /> </> )
+  return (
+    <RoutesNavigator />
+  );
 }
--- react-native/a-rosa-je-expo/routes/RoutesStack.js
+++ react-native/a-rosa-je-expo/routes/RoutesStack.js
@@ -3,6 +3,9 @@ import { createAppContainer } from "react-navigation";
 import SignupForm from '../screens/Signup';
 import MainPage from '../screens/Accueil';
 import LoginScreen from '../screens/Login';
+import MyPlantsScreen from "../screens/MyPlantsScreen";
+import CameraScreen from "../screens/CameraScreen";
+
 
 
 const screens = {
@@ -17,7 +20,17 @@ const screens = {
     Signup: {
         screen: SignupForm
     },
-    
+    MyPlants: {
+        screen: MyPlantsScreen,
+        navigationOptions: {
+          title: "Mes plantes",
+          // headerStyle: { backgroundColor: "lightGreen" },
+        },
+      },
+      Camera: {
+        screen: CameraScreen,
+        navigationOptions: { title: "Prendre une photo" },
+      },
 };
 
 const RoutesStack = createStackNavigator(screens);
